8fb086e546bfe55b1e59680e7d17bb3d3d269f49
brodyns <118648344+brodyns@users.noreply.github.com>
2023-12-12 23:43:33 +0800

    102. 二叉树的层序遍历

---
+++ Main.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        System.out.println((new Solution()).levelOrder(root));
+    }
+}
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null){
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            List<Integer> level = new ArrayList<>();
+            int currentLevelSize = queue.size();
+            for (int i = 0; i < currentLevelSize; i++) {
+                TreeNode cur = queue.poll();
+                level.add(cur.val);
+                if(cur.left != null){
+                    queue.offer(cur.left);
+                }
+                if(cur.right != null){
+                    queue.offer(cur.right);
+                }
+            }
+            res.add(level);
+        }
+        return res;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
